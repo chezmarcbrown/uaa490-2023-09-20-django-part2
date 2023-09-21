db934d007c1455ad02889f5b17abee9092b6357c
acslmarc <marc@chezbrown.org>
2023-09-20 17:26:32 -0800

    end of class wed 9/20

--- hello/urls.py
+++ hello/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("marc/", views.marc, name="marc"),
+    path("uaa490/", views.uaa490, name="xyz"),
+    path("<str:name>/", views.greet, name="greet"),
+
 ]
